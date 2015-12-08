80196ba5bab1d92d4ea2680150ae3b0c779a10d1
Sally Kong <kongsally@users.noreply.github.com>
2015-12-07 19:45:49 -0500

    more pragma names

--- glsl/quad.vert.glsl
+++ glsl/quad.vert.glsl
@@ -6,6 +6,7 @@ attribute vec3 a_position;
 
 varying vec2 v_uv;
 
+#pragma name quad
 void main() {
     gl_Position = vec4(a_position, 1.0);
     v_uv = a_position.xy * 0.5 + 0.5;
--- glsl/red.frag.glsl
+++ glsl/red.frag.glsl
@@ -2,6 +2,7 @@
 precision highp float;
 precision highp int;
 
+#pragma name scissor_debug_white
 void main() {
     gl_FragColor = vec4(0.1, 0.1, 0.1, 0.1);
 }
